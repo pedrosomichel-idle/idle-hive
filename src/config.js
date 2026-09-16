0f459dabf49f1a279210ba7056a77699c204314d
unknown <mb.pedroso7@gmail.com>
2026-09-16 19:07:50 -0300

    nome fixo do instalador

--- src/config.js
+++ src/config.js
@@ -11,5 +11,5 @@
 module.exports = {
   SUPABASE_URL: 'https://zcjpebrokrcemnrzpvaf.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjanBlYnJva3JjZW1ucnpwdmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0ODAxNDYsImV4cCI6MjEwNTA1NjE0Nn0.qEfKT7Y9HfjzT7EccpHrZy-WcuPgd7_gfKHuf72YfTA',
-  BACKEND_URL: 'http://localhost:3000',
+  BACKEND_URL: 'https://idle-hive-backend.vercel.app',
 };
